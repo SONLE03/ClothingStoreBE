41a04e5206a94fd56808659935f5b1324628d763
SONLE03 <sonle102003@gmail.com>
2024-11-25 23:53:15 +0700

    complete data model

---
+++ ClothingStoreBENET/Models/BaseEntity.cs
@@ -0,0 +1,34 @@
+﻿using NuGet.Packaging.Signing;
+using System;
+using System.Reflection.Metadata;
+
+namespace FurnitureStoreBE.Models
+{
+    public class BaseEntity
+    {
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public string? CreatedBy { get; set; }
+        public string? UpdatedBy { get; set; }
+        public bool IsDeleted { get; set; } = false;
+        public DateTime? DeleteDate { get; set; }
+
+        public void setCommonCreate(string currentLoginId)
+        {
+            this.CreatedDate = resultTimestamp();
+            this.CreatedBy = currentLoginId;
+            this.UpdatedDate = resultTimestamp();
+            this.UpdatedBy = currentLoginId;
+        }
+        public void setCommonUpdate(string currentLoginId)
+        {
+            this.UpdatedDate = resultTimestamp();
+            this.UpdatedBy = currentLoginId;
+        }
+
+        public static DateTime resultTimestamp()
+        {
+            return DateTime.UtcNow;
+        }
+    }
+}
---
+++ ClothingStoreBENET/Models/Size.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FurnitureStoreBE.Models
+{
+    [Table("Size")]
+    public class Size : BaseEntity
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string SizeName { get; set; }
+        public ICollection<ProductVariant>? ProductVariants { get; set; }
+    }
+}
